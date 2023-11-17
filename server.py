0031e1f2d421db25994760afbd8faf8861d13664
Waseem Akram <hackerbad669@gmail.com>
2023-11-17 12:56:47 +0500

    Add error handling and response formatting to sentiment analysis API

--- server.py
+++ server.py
@@ -20,8 +20,9 @@ handle increased load, making it suitable for real-world applications.
 
 Author: Waseem Akram (https://github.com/evildevill/)
 """
-# 
-from flask import Flask, render_template, request
+ 
+from flask import Flask, render_template, request, jsonify
+
 from EmotionDetection.emotion_detection import emotion_detector
 from EmotionDetection.emotion_detection import emotion_predictor
 
@@ -39,16 +40,24 @@ def sent_detector():
     Analyze the user-provided text for emotions and return the result.
     """
     text_to_detect = request.args.get('textToAnalyze')
+    
+    if not text_to_detect:
+        return jsonify({'error': 'Invalid request. Please provide textToAnalyze parameter.'}), 400
+
     response = emotion_detector(text_to_detect)
     formated_response = emotion_predictor(response)
+
     if formated_response['dominant_emotion'] is None:
-        return "Invalid text! Please try again."
-    return (
-        f"For the given statement, the system response is 'anger': {formated_response['anger']} "
-        f"'disgust': {formated_response['disgust']}, 'fear': {formated_response['fear']}, "
-        f"'joy': {formated_response['joy']} and 'sadness': {formated_response['sadness']}. "
-        f"The dominant emotion is {formated_response['dominant_emotion']}."
-    )
+        return jsonify({'error': 'Invalid text! Please try again.'}), 500
+
+    return jsonify({
+        'anger': formated_response['anger'],
+        'disgust': formated_response['disgust'],
+        'fear': formated_response['fear'],
+        'joy': formated_response['joy'],
+        'sadness': formated_response['sadness'],
+        'dominant_emotion': formated_response['dominant_emotion']
+    })
 
 @app.route("/")
 def render_index_page():
@@ -56,6 +65,11 @@ def render_index_page():
         page over the Flask channel
     '''
     return render_template('index.html')
+
+# Custom error handler for 500 Internal Server Error
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == "__main__":
     run_emotion_detection()
