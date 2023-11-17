2097bf908eda15ed71bb4ad2fc24ced80bfad5f9
Waseem Akram <hackerbad669@gmail.com>
2023-11-17 12:33:37 +0500

    Fix syntax errors and update API endpoint in emotion detection and web script files

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,8 +3,8 @@ import json
 
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = { "raw_document": { "text": text_to_analyze } }
+    Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    Input json: { "raw_document": { "text": text_to_analyse } }
     response = requests.post(URL, json = input_json, headers=header)
     formated_response = json.loads(response.text)
 
--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,34 +1,34 @@
-const runSentimentAnalysis = async () => {
-    const textToAnalyze = document.getElementById("textToAnalyze").value;
+// const runSentimentAnalysis = async () => {
+//     const textToAnalyze = document.getElementById("textToAnalyze").value;
 
-    try {
-        const response = await fetch(`emotionDetector?textToAnalyze=${textToAnalyze}`);
+//     try {
+//         const response = await fetch(`emotionDetector?textToAnalyze=${textToAnalyze}`);
         
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-
-        const result = await response.text();
-        document.getElementById("system_response").innerHTML = result;
-    } catch (error) {
-        console.error("Error during sentiment analysis:", error.message);
-        // Handle the error, e.g., display a user-friendly message to the user
-    }
-};
-
-// Example of calling the function when a button is clicked
-document.getElementById("analyzeButton").addEventListener("click", runSentimentAnalysis);
-
-
-// let RunSentimentAnalysis = ()=>{
-//     textToAnalyze = document.getElementById("textToAnalyze").value;
-
-//     let xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//             document.getElementById("system_response").innerHTML = xhttp.responseText;
+//         if (!response.ok) {
+//             throw new Error(`HTTP error! Status: ${response.status}`);
 //         }
-//     };
-//     xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
-//     xhttp.send();
-// }
+
+//         const result = await response.text();
+//         document.getElementById("system_response").innerHTML = result;
+//     } catch (error) {
+//         console.error("Error during sentiment analysis:", error.message);
+//         // Handle the error, e.g., display a user-friendly message to the user
+//     }
+// };
+
+// // Example of calling the function when a button is clicked
+// document.getElementById("analyzeButton").addEventListener("click", runSentimentAnalysis);
+
+
+let RunSentimentAnalysis = ()=>{
+    textToAnalyze = document.getElementById("textToAnalyze").value;
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("system_response").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
+    xhttp.send();
+}
